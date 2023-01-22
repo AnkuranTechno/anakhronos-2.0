f8780284a9efde91c728af8f918cb36df405f797
Akash <ag308669@gmail.com>
2023-01-22 22:31:42 +0530

    Added github pages

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
 import './Navbar.scss';
+import { navbarLogo } from '../../constants/image';
 
 const Navbar = () => {
   return (
     <header className="app__section navbar">
       <div className="app__section-container navbar-container">
-        <h1>Navbar</h1>
+        <div className="navbar__logo-container">
+          <div className="navbar__logo">
+            <img src={navbarLogo} alt="logo" />
+          </div>
+        </div>
+        <ul className="navbar__menu">
+          <li className="navbar__menu-item">
+            <a href="/">Home</a>
+          </li>
+          <li className="navbar__menu-item">
+            <a href="/">About</a>
+          </li>
+          <li className="navbar__menu-item">
+            <a href="/">Contact</a>
+          </li>
+        </ul>
       </div>
     </header>
   );
