dc2865dec700481b38375d4623381dd5e069cf22
souviksaha27 <souviksahamay27@gmail.com>
2023-02-01 00:17:11 +0530

    sponsors

--- src/sections/Sponsors/Sponsors.jsx
+++ src/sections/Sponsors/Sponsors.jsx
@@ -6,9 +6,16 @@ const Sponsors = () => {
   return (
     <div className="app__section sponsors">
       <div className="app__section-container sponsors-container">
-        <h1>Sponsors</h1>
-      </div>
-    </div>
+        <h1>Our sponsors</h1>
+        <div className="sponsor__images">
+
+            <img src='nestle.png' alt='chevrolet'/>
+            <img src='ola-cabs.png' alt='chevrolet'/>
+            <img src='the-times-of-india-logo.png' alt='chevrolet'/>
+            <img src='ola-cabs.png' alt='chevrolet'/>
+            </div>
+          </div>
+        </div>   
   );
 };
 
