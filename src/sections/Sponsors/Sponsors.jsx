0f3b2b0d70c410c9addede6bd1f76275166d2125
souviksaha27 <souviksahamay27@gmail.com>
2023-02-05 21:23:55 +0530

    Sponsors and About section done

--- src/sections/Sponsors/Sponsors.jsx
+++ src/sections/Sponsors/Sponsors.jsx
@@ -7,15 +7,30 @@ const Sponsors = () => {
     <div className="app__section sponsors" id="sponsors">
       <div className="app__section-container sponsors-container">
         <h1>Our sponsors</h1>
-        <div className="sponsor__images">
+        <div class="sponsors-slideshow">
+          <div class="slide">
+            {/* to add a new image/sponsor you need to add it in all three images chunks. No need to resize the image, it is taken care of in the css. */}
+            <img src='nestle.png' alt='chevrolet' />   
+            <img src='ola-cabs.png' alt='chevrolet' />
+            <img src='the-times-of-india-logo.png' alt='chevrolet' />
+            <img src='ola-cabs.png' alt='chevrolet' />
+            <img src='the-times-of-india-logo.png' alt='chevrolet' />
 
-            <img src='nestle.png' alt='chevrolet'/>
-            <img src='ola-cabs.png' alt='chevrolet'/>
-            <img src='the-times-of-india-logo.png' alt='chevrolet'/>
-            <img src='ola-cabs.png' alt='chevrolet'/>
-            </div>
+            <img src='nestle.png' alt='chevrolet' />
+            <img src='ola-cabs.png' alt='chevrolet' />
+            <img src='the-times-of-india-logo.png' alt='chevrolet' />
+            <img src='ola-cabs.png' alt='chevrolet' />
+            <img src='the-times-of-india-logo.png' alt='chevrolet' />
+
+            <img src='nestle.png' alt='chevrolet' />
+            <img src='ola-cabs.png' alt='chevrolet' />
+            <img src='the-times-of-india-logo.png' alt='chevrolet' />
+            <img src='ola-cabs.png' alt='chevrolet' />
+            <img src='the-times-of-india-logo.png' alt='chevrolet' />
           </div>
-        </div>   
+        </div>
+      </div>
+    </div>
   );
 };
 
