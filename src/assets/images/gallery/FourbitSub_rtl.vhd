611acc9cf3ee3aac598c57c435e326ceacf55867
Asif Ali <asif.a.7277@gmail.com>
2023-03-04 15:51:35 +0530

    changes in the gallery section

---
+++ src/assets/images/gallery/FourbitSub_rtl.vhd
@@ -0,0 +1,98 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   18:46:13 03/04/2023
+-- Design Name:   
+-- Module Name:   C:/Users/Hp/OneDrive/Documents/asif/fourbitsub/fbs_test.vhd
+-- Project Name:  fourbitsub
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: bitsub_rtl
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY fbs_test IS
+END fbs_test;
+ 
+ARCHITECTURE behavior OF fbs_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT bitsub_rtl
+    PORT(
+         A : IN  std_logic_vector(3 downto 0);
+         B : IN  std_logic_vector(3 downto 0);
+         Cin : IN  std_logic;
+         Diff : OUT  std_logic_vector(3 downto 0);
+         Borrow : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal A : std_logic_vector(3 downto 0) := (others => '0');
+   signal B : std_logic_vector(3 downto 0) := (others => '0');
+   signal Cin : std_logic := '0';
+
+ 	--Outputs
+   signal Diff : std_logic_vector(3 downto 0);
+   signal Borrow : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: bitsub_rtl PORT MAP (
+          A => A,
+          B => B,
+          Cin => Cin,
+          Diff => Diff,
+          Borrow => Borrow
+        );
+
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+     A<="0101";
+	  B<="0011";
+	  Cin<='0';
+	  wait for 1 ps;
+	   A<="0111";
+	  B<="0011";
+	  Cin<='1';
+	  wait for 1 ps;
+	   A<="0101";
+	  B<="0010";
+	  Cin<='0';
+	  wait for 1 ps;
+	   A<="1100";
+	  B<="0011";
+	  Cin<='0';
+	  wait for 1 ps;
+   end process;
+
+END;
--- src/sections/Gallery/Gallery.jsx
+++ src/sections/Gallery/Gallery.jsx
@@ -1,150 +1,153 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
 import React from "react";
 import "./Gallery.scss";
-// import {
-//   gallimg1,
-//   gallimg2,
-//   gallimg3,
-//   gallimg4,
-//   gallimg5,
-//   gallimg6,
-//   gallimg7,
-//   gallimg8,
-//   gallimg9,
-//   gallimg10,
-//   gallimg11,
-//   gallimg12,
-//   gallimg13,
-//   gallimg14,
-//   gallimg15,
-//   gallimg16,
-//   gallimg17,
-//   gallimg18,
-//   gallimg19,
-//   gallimg20,
-//   gallimg21,
-//   gallimg22,
-//   gallimg23,
-//   gallimg24,
-//   gallimg25,
-//   gallimg26,
-//   gallimg27,
-// } from "../../constants/images";
+import {
+  gallimg1,
+  gallimg2,
+  gallimg3,
+  gallimg4,
+  gallimg5,
+  gallimg6,
+  gallimg7,
+  gallimg8,
+  gallimg9,
+  gallimg10,
+  gallimg11,
+  gallimg12,
+  gallimg13,
+  gallimg14,
+  gallimg15,
+  gallimg16,
+  gallimg17,
+  gallimg18,
+  gallimg19,
+  gallimg20,
+  gallimg21,
+  gallimg22,
+  gallimg23,
+  gallimg24,
+  gallimg25,
+  gallimg26,
+  gallimg27,
+} from "../../constants/images";
 
-// const data = [
-//   {
-//     img: gallimg1,
-//     id: 1,
-//   },
-//   {
-//     img: gallimg2,
-//     id: 2,
-//   },
-//   {
-//     img: gallimg3,
-//     id: 3,
-//   },
-//   {
-//     img: gallimg4,
-//     id: 4,
-//   },
-//   {
-//     img: gallimg5,
-//     id: 5,
-//   },
-//   {
-//     img: gallimg6,
-//     id: 6,
-//   },
-//   {
-//     img: gallimg7,
-//     id: 7,
-//   },
-//   {
-//     img: gallimg8,
-//     id: 8,
-//   },
-//   {
-//     img: gallimg9,
-//     id: 9,
-//   },
-//   {
-//     img: gallimg10,
-//     id: 10,
-//   },
-//   {
-//     img: gallimg11,
-//     id: 11,
-//   },
-//   {
-//     img: gallimg12,
-//     id: 12,
-//   },
-//   {
-//     img: gallimg13,
-//     id: 13,
-//   },
-//   {
-//     img: gallimg14,
-//     id: 14,
-//   },
-//   {
-//     img: gallimg15,
-//     id: 15,
-//   },
-//   {
-//     img: gallimg16,
-//     id: 16,
-//   },
-//   {
-//     img: gallimg17,
-//     id: 17,
-//   },
-//   {
-//     img: gallimg18,
-//     id: 18,
-//   },
-//   {
-//     img: gallimg19,
-//     id: 19,
-//   },
-//   {
-//     img: gallimg20,
-//     id: 20,
-//   },
-//   {
-//     img: gallimg21,
-//     id: 21,
-//   },
-//   {
-//     img: gallimg22,
-//     id: 22,
-//   },
-//   {
-//     img: gallimg23,
-//     id: 23,
-//   },
-//   {
-//     img: gallimg24,
-//     id: 24,
-//   },
-//   {
-//     img: gallimg25,
-//     id: 25,
-//   },
-//   {
-//     img: gallimg26,
-//     id: 26,
-//   },
-//   {
-//     img: gallimg27,
-//     id: 27,
-//   },
-// ];
+const data = [
+  {
+    img: gallimg1,
+    id: 1,
+  },
+  {
+    img: gallimg2,
+    id: 2,
+  },
+  {
+    img: gallimg3,
+    id: 3,
+  },
+  {
+    img: gallimg4,
+    id: 4,
+  },
+  {
+    img: gallimg5,
+    id: 5,
+  },
+  {
+    img: gallimg6,
+    id: 6,
+  },
+  {
+    img: gallimg7,
+    id: 7,
+  },
+  {
+    img: gallimg8,
+    id: 8,
+  },
+  {
+    img: gallimg9,
+    id: 9,
+  },
+  {
+    img: gallimg10,
+    id: 10,
+  },
+  {
+    img: gallimg11,
+    id: 11,
+  },
+  {
+    img: gallimg12,
+    id: 12,
+  },
+  {
+    img: gallimg13,
+    id: 13,
+  },
+  {
+    img: gallimg14,
+    id: 14,
+  },
+  {
+    img: gallimg15,
+    id: 15,
+  },
+  {
+    img: gallimg16,
+    id: 16,
+  },
+  {
+    img: gallimg17,
+    id: 17,
+  },
+  {
+    img: gallimg18,
+    id: 18,
+  },
+  {
+    img: gallimg19,
+    id: 19,
+  },
+  {
+    img: gallimg20,
+    id: 20,
+  },
+  {
+    img: gallimg21,
+    id: 21,
+  },
+  {
+    img: gallimg22,
+    id: 22,
+  },
+  {
+    img: gallimg23,
+    id: 23,
+  },
+  {
+    img: gallimg24,
+    id: 24,
+  },
+  {
+    img: gallimg25,
+    id: 25,
+  },
+  {
+    img: gallimg26,
+    id: 26,
+  },
+  {
+    img: gallimg27,
+    id: 27,
+  },
+];
 const Gallery = () => {
   return (
     <div className="app__section gallery" id="gallery">
       <h1 className="app__section-container">Gallery</h1>
-      {/* <div className=" gallery__container">
+      <div className=" gallery__container">
         {data.map((item, id) => {
           return (
             <div className="item" key={id}>
@@ -158,7 +161,7 @@ const Gallery = () => {
             </div>
           );
         })}
-      </div> */}
+      </div>
     </div>
   );
 };
